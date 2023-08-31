0af06311b338d7d007594de66059ff96f2ce623a
suzaankhan <92640978+suzaankhan@users.noreply.github.com>
2023-08-31 23:38:05 +0530

    added weatherApp.js

---
+++ weatherApp.js
@@ -0,0 +1,65 @@
+let called = true;
+        function setBgImg(wthr_type){
+            document.body.style.backgroundImage = `url('${wthr_type}.jpg')`;
+            document.body.style.backgroundSize = "100vw 100vh";
+            document.body.style.opacity = "80%";
+        }
+
+        function find_wh(){
+            cityName = document.getElementById("inp").value;
+            
+            apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=8e2d4a97e941c0022fa82bc3e886d0dd`;
+            fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => {
+                console.log("entered api");
+            // HAndle the data returned form the api
+            temp = (parseInt(data.main.temp) - 273);
+            tempString = `${(parseInt(data.main.temp) - 273)} °C`;
+            city = data.name;
+            humidity = (data.main.humidity);
+            wind_speed = `${data.wind.speed} miles/hr`;
+            wthr = data.weather[0].main;
+            console.log(wthr); 
+            console.log(typeof wthr); 
+            // console.log("This is in lower" + wthr.toLowercase());
+            if(wthr.localeCompare("Clear") == 0 ){
+                setBgImg("Clear");
+            }
+            else if(wthr.localeCompare("Haze") == 0){
+                setBgImg("Haze");
+            }
+            else if(parseInt(temp) <= 2){
+                setBgImg("cold");
+            }
+            else if(wthr.localeCompare("Clouds") == 0){
+                setBgImg("Clouds");
+            }
+            else if(wthr.localeCompare("Rain") == 0){
+                setBgImg("Rain");
+            }
+            else if(wthr.localeCompare("Mist") == 0){
+                setBgImg("mist");
+            }
+            else{
+                alert("error occured in adding images");
+            }
+            
+            if(called){
+                document.getElementById("info").remove();
+            }
+            called = false;
+            // document.getElementById("temp").innerText = tempString;
+            document.getElementById("cityname_temp").innerText = `${city}: ${tempString}`;
+            document.getElementById("weather").innerText = `Weather: ${wthr}`;
+            document.getElementById("humidity").innerText = `Humidity: ${humidity}`;
+            document.getElementById("wind_speed").innerText = `Wind Speed: ${wind_speed}`;
+            
+        })
+        .catch(error => {
+            //Handle errors if the request fails
+            window.alert("Some error occured");
+            console.log(error);
+        });
+        }
+
